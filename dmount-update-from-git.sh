83f5485ec7d10808db1d3e24ea788c7c4758b8b8
LABB <unt@labb.email>
2019-03-10 11:25:19 -0400

    Fix installation script

--- dmount-update-from-git.sh
+++ dmount-update-from-git.sh
@@ -16,7 +16,32 @@ git clone https://github.com/abbluiz/dmount.git $HOME/.dmount-tmp
 
 # TODO: diff
 
-$HOME/.dmount-tmp/dmount-install.sh
+installPath="/usr/local/bin"
+
+script[0]="dmount-update-from-git"
+script[1]="dmount-uninstall"
+script[2]="dmount"
+script[3]="dunmount"
+
+updatingOrInstalling() {
+
+    if [ -x "$2/$3" ]; then
+        report="Updating"
+    else
+        report="Installing"
+    fi
+
+}
+
+for i in ${script[@]}
+do 
+
+    report="Installing/Updating"
+    updatingOrInstalling $report $installPath $i
+    echo "$report $i..."
+    install "$HOME/.dmount-tmp/$i.sh" "$installPath/$i"
+
+done
 
 echo "Deleting temporary directory ~/.dmount-tmp"
 rm -rf $HOME/.dmount-tmp
